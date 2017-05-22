0aafaee351a5be07cd646b87a6d66b664ec8fb63
YOUNG <young@YOUNGs-MacBook-Pro.local>
2017-05-22 19:48:52 +0300

    Convert .mov to .jpeg

---
+++ VideoToImages.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env Python3
+
+'''
+SfMPipeline/VideoToImages.py
+
+'''
+
+__author__ = "Seyoung Park"
+__copyright__ = "Copyright 2017, Seyoung Park"
+__credits__ = ["Seyoung Park"]
+__license__ = "MIT"
+__version__ = "1.0.1"
+__maintainer__ = "Seyoung Park"
+__email__ = "seyoung.arts.park@protonmail.com"
+__status__ = "Production"
+
+
+import cv2
+import os
+
+VIDEO_PATH = '/Users/young/Downloads/IMG_4188.MOV'
+FRAMES_PATH_PREFIX = 'frames'
+
+vidcap = cv2.VideoCapture(VIDEO_PATH)
+success,image = vidcap.read()
+count = 0
+success = True
+
+
+def ensure_dir():
+    frames_dir = VIDEO_PATH.split('/')[-1].split('.')[0]
+    frames_dir = os.path.join(FRAMES_PATH_PREFIX, frames_dir)
+    if not os.path.exists(frames_dir):
+        os.makedirs(frames_dir)
+    return frames_dir
+
+frames_dir = ensure_dir()
+
+while success:
+  success,image = vidcap.read()
+  print 'Read a new frame: ', success
+  write_path = os.path.join(frames_dir, "frame%d.jpg" % count)
+  cv2.imwrite(write_path, image)     # save frame as JPEG file
+  count += 1
